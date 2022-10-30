9e150c106f620b3e8829ae15c6cfa719a0f62de4
auliaucheni <ulfaauliacheni@gmail.com>
2022-10-30 16:53:10 +0700

    menambahkan file

---
+++ MODUL 2 ULFA/ulfa_Booking.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BOOKING | Ulfa_1202204019</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+
+    $jenismobil = [
+        [
+            "mobil" => "Toyota Rush",
+            "img" => "img/toyota rush.png"
+        ],
+        [
+            "mobil" => "Toyota Ayla",
+            "img" => "img/ayla.png"
+        ],
+        [
+            "mobil" => "Honda Brio",
+            "img" => "img/brio.png"
+        ],
+    ]
+
+    ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse justify-content-md-center">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="ulfa_Home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="ulfa_Booking.php">Booking</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <h1 style="text-align:center; font-size: 25px">Rent your car now!</h1>
+    <div class="container card shadow-sm">
+        <div class="d-flex justify-content-arround">
+            <div class="col-6 ps-5 pe-5 d-flex align-items-center">
+                <?php
+                if (isset($_GET["img"])) {
+                ?><img class="" width="100%" src="<?= $_GET["img"]; ?>" alt="">
+                <?php
+                } else {
+                ?><img width="100%" src="toyota rush.png" alt="">
+                <?php } ?>
+            </div>
+            <div class="col-6 ps-5 pe-5">
+                <ul type="none">
+                    <form action="ulfa_MyBooking.php" method="post">
+                        <li class="m-1">
+                            <label for="name">Name</label><br>
+                            <input class="form-control" type="text" name="name" id="name" value="Ulfa_1202204019" readonly>
+                        </li>
+                        <li class="m-1">
+                            <label for="booking">Book Date</label><br>
+                            <input class="form-control" type="date" name="booking" id="booking">
+                        </li>
+                        <li class="m-1">
+                            <label for="waktu">Start Time</label><br>
+                            <input class="form-control" type="time" name="waktu" id="waktu">
+                        </li>
+                        <li class="m-1">
+                            <label for="day">Duration (Days)</label><br>
+                            <input class="form-control" type="number" name="day" id="day" min="1">
+                        </li>
+                        <li class="m-1">
+                            <label for="mobil">Car Type</label>
+                            <select class="form-select" name="mobil" id="mobil">
+                                <option selected>Choose...</option>
+                                <?php foreach ($jenismobil as $mobil) : ?>
+                                    <?php
+                                    if ($mobil["mobil"] == $_GET["mobil"]) {
+                                    ?><option selected="selected" value="<?= $mobil["mobil"]; ?>"><?= $mobil["mobil"]; ?></option>
+                                    <?php
+                                    } else {
+                                    ?><option value="<?= $mobil["mobil"]; ?>"><?= $mobil["mobil"]; ?></option>
+                                    <?php } ?>
+                                <?php endforeach; ?>
+                            </select>
+                        </li>
+                        <li class="m-1">
+                            <label for="phone">Phone Number</label><br>
+                            <input class="form-control" type="number" name="phone" id="phone" min="0">
+                        </li>
+                        <li class="m-1">
+                            <label for="service">Add Service(s)</label><br>
+                            <input class="form-check-input m-1" type="checkbox" name="ser[]" value="Health protocol" id="health"><label class="form-check-label" for="health">Health protocol / Rp25.000</label><br>
+                            <input class="form-check-input m-1" type="checkbox" name="ser[]" value="Driver" id="driver"><label class="form-check-label" for="driver">Driver / Rp100.000</label><br>
+                            <input class="form-check-input m-1" type="checkbox" name="ser[]" value="Fuel Filled" id="fuel"><label class="form-check-label" for="fuel">Fuel Filled / Rp250.000</label><br>
+                        </li>
+                        <li class="text-center">
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-success justify-content-around" type="submit">Book</button>
+                            </div>
+                        </li>
+                    </form>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <footer class="fixed-bottom text-center pt-3 bg-light">
+        <p style="text-align: center; color: lightslategray">
+            Created by ULFA_1202204019
+        </p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ MODUL 2 ULFA/ulfa_Home.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HOME | Ulfa_1202204019</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse justify-content-md-center">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="ulfa_Home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ulfa_Booking.php">Booking</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <h1 style="text-align:center; font-size: 25px">WELCOME TO EAD RENT</h1>
+  <p style="text-align:center;">Find your best deal, here!</p>
+  <div class="d-flex justify-content-around">
+    <div class="card" style="width: 18rem;">
+      <img src="toyota rush.png" class="card-img-top" alt="" style="width:235px">
+      <div class="card-body">
+        <h5 class="card-title">Toyota Rush</h5>
+        <p class="card-text" style="color: grey;">Rp 200000/Day</p>
+      </div>
+      <ul class="list-group list-group-flush" style="text-align: center;">
+        <li class="list-group-item" style="color: blue; font-weight:bold;">7 Kursi</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">1500 CC</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">Manual</li>
+      </ul>
+      <div class="d-flex justify-content-center">
+        <form action="ulfa_Booking.php">
+          <button type="submit" value="toyota rush.png" name="img" class="btn btn-primary">Book Now</button>
+        </form>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+      <img src="ayla.png" class="card-img-top" alt="" style="width:235px">
+      <div class="card-body">
+        <h5 class="card-title">Toyota Ayla</h5>
+        <p class="card-text" style="color: grey;">Rp 150000/Day</p>
+      </div>
+      <ul class="list-group list-group-flush" style="text-align: center;">
+        <li class="list-group-item" style="color: blue; font-weight:bold;">5 Kursi</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">1200 CC</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">Manual</li>
+      </ul>
+      <div class="d-flex justify-content-center">
+        <form action="ulfa_Booking.php">
+          <button type="submit" value="ayla.jpg" name="img" class="btn btn-primary">Book Now</button>
+        </form>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+      <img src="brio.png" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Honda Brio</h5>
+        <p class="card-text" style="color: grey;">Rp 150000/Day</p>
+      </div>
+      <ul class="list-group list-group-flush" style="text-align: center;">
+        <li class="list-group-item" style="color: blue; font-weight:bold;">5 Kursi</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">1200 CC</li>
+        <li class="list-group-item" style="color: blue; font-weight:bold;">Automatic</li>
+      </ul>
+      <div class="d-flex justify-content-center">
+        <form action="ulfa_Booking.php">
+          <button type="submit" value="brio.png" name="img" class="btn btn-primary">Book Now</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer class="fixed-bottom text-center pt-3 bg-light">
+    <p style="text-align: center; color: lightslategray">
+      Created by ULFA_1202204019
+    </p>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
